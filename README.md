08f5e0372655dbd5892f37d30d8f6074fecbdeff
cherro <2221619190@qq.com>
2021-03-10 07:47:55 -0800

    hive

---
+++ README.md
@@ -0,0 +1,7 @@
+# Recommend System
+
+## 架构设计参考
+
+![](pics/1.jpeg)![](pics/2.png)![](pics/3.png)![](pics/4.png)
+
+## 基础设施搭建
---
+++ docker/config/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo -e "\n"
+
+$HADOOP_HOME/sbin/start-dfs.sh
+
+echo -e "\n"
+
+$HADOOP_HOME/sbin/start-yarn.sh
+
+echo -e "\n"
+
+$HIVE_HOME/bin/schematool -dbType mysql -initSchema
+
+echo -e "\n"
